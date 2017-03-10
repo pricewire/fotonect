7aa9f4cc4e564fc7308fff8523cc4cf0dba40883
Mark <mark@comcast.net>
2017-03-10 13:49:43 -0500

    Initial Commit

---
+++ fotonect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fotonect
+//
+//  Created by Mark Langlois on 3/10/17.
+//  Copyright © 2017 Mark Langlois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
